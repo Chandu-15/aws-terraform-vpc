05889abf299e463632d0e2d2290559f0cb505ab6
Chandana <rebbachandana54@gmail.com>
2026-01-22 22:48:02 +0000

    s

--- main.tf
+++ main.tf
@@ -120,7 +120,7 @@ resource "aws_route_table_association" "database"{
 }
 
 resource "aws_route" "public"{
-    route_table_id=aws_route_table.database.id
+    route_table_id=aws_route_table.public.id
     destination_cidr_block="0.0.0.0/0"
     gateway_id=aws_internet_gateway.main.id
 }
