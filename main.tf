9a6c548cec8da275132db3ef10d03fafbec70195
Chandana <rebbachandana54@gmail.com>
2026-01-22 20:39:41 +0000

    s

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "main" {
     cidr_block= var.vpc_cidr
     instance_tenancy= "default"
-    enable_host_names= true
+    enable_dns_hostnames= true
     tags= merge(
         local.common_tags , 
        {Name="${local.common_name}-dev-vpc"}
